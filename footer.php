05e296eb0180ec3589f49b4067042a4eeed59fde
unknown <rmateescu@gamingcampus.local>
2021-12-01 09:51:55 +0100

    footer 2.0

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <div class="divider"></div>
+<div class="footer-content">
     <!-- Présentations de créateurs -->
     <div class="row">
       <div class="col s12 l4 offset-l1">
@@ -24,3 +25,4 @@
         </div>
       </div>
     </div>
+</div>
